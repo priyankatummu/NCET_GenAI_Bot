6f1e234a81a0492431516378614975cc9b7bb190
T Priyanka <priyankatummu9353@gmail.com>
2026-04-15 11:57:36 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from groq import Groq
 
 st.set_page_config("pragyanAI Content Generator", layout="wide")
 st.title("pragyan_AI - Content Generator")
-st.image("Screenshot (11).png")
+st.image("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/71322cb8-c781-4c3a-b2b8-58ac33324fe6/dfof921-61b056f7-43fe-4436-9132-103ccb628712.jpg/v1/fill/w_1192,h_670,q_70,strp/tom_and_jerry_chase__by_xrthayp44_dfof921-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzcxMzIyY2I4LWM3ODEtNGMzYS1iMmI4LTU4YWMzMzMyNGZlNlwvZGZvZjkyMS02MWIwNTZmNy00M2ZlLTQ0MzYtOTEzMi0xMDNjY2I2Mjg3MTIuanBnIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.ZhEU9B_KpYqaqJM26kMxAcQI4lc98VHtcL1rN5QV7os")
 # Get GROQ API Key
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 # Get Product Name and Audience for That Product
